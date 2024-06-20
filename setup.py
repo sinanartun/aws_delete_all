323ea77cbedd57f4f98585f182d38a8dc4b5538d
”sinanartun” <sinanartun@gmail.com>
2024-06-21 00:41:57 +0200

    update

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as fh:
+with open("readme.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
