2aa9c7f035745b4e1f3957988aacbca89e75607a
”sinanartun” <26043673+sinanartun@users.noreply.github.com>
2024-12-27 19:02:47 +0100

    Bump version to 0.5.48 in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("readme.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='aws-delete-all',
-    version='0.5.47',
+    version='0.5.48',
     author='Sinan Artun',
     author_email='sinanartun@gmail.com',
     description='A script that concurrently deletes common AWS resources like S3 buckets, RDS instances, and EC2 instances across all AWS regions.',
